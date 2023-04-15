b1e5cf508e8bdf4f756dadb0e9ba9e9ee0eb6c12
Alexandr <Chur10@mail.ru>
2023-04-15 18:05:10 +0200

    added login and pass .env. autorization process

--- endorse.py
+++ endorse.py
@@ -1,3 +1,27 @@
+import time
+
+from environs import Env
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 
-link = ""
+link = "https://www.linkedin.com/in/aleksandr-churakov/"
+browser = webdriver.Chrome()
+env = Env()
+env.read_env(".env")
+
+try:
+    browser.get("https://linkedin.com")
+    login_input = browser.find_element(By.CSS_SELECTOR, "#session_key")
+    login_input.clear()
+    login_input.send_keys(env.str("LINKEDIN_LOGIN"))
+    password_input = browser.find_element(By.CSS_SELECTOR, "#session_password")
+    password_input.clear()
+    password_input.send_keys(env.str("LINKEDIN_PASSWORD"))
+    # browser.get(f"{link.rstrip('/')}/details/skills/")
+    time.sleep(10)
+except Exception as ex:
+    print(ex)
+finally:
+    browser.quit()
+
